95043ad1e7c53e7e6331ecd3fc3d80357e90c660
Neil Wang <neil.wang@curai.com>
2024-04-16 18:13:09 -0400

    chore(demo): Select scrollbarWidth error

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -3,53 +3,129 @@ import {
   Button,
   H1,
   Paragraph,
+  Select,
   Separator,
   Sheet,
   useToastController,
   XStack,
   YStack,
+  Adapt,
+  getFontSize,
 } from '@my/ui'
-import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
-import { useState } from 'react'
+import { LinearGradient } from 'tamagui/linear-gradient'
+
+import type { FontSizeTokens, SelectProps } from 'tamagui'
+import { useMemo, useState } from 'react'
+
+import { ChevronDown, ChevronUp, Check } from '@tamagui/lucide-icons'
 import { useLink } from 'solito/link'
+const items = [{ name: 'Apple' }]
 
 export function HomeScreen() {
   const linkProps = useLink({
     href: '/user/nate',
   })
+  const [val, setVal] = useState('apple')
 
   return (
     <YStack f={1} jc="center" ai="center" p="$4" gap="$4">
-      <YStack gap="$4" bc="$background">
-        <H1 ta="center">Welcome to Tamagui.</H1>
-        <Paragraph ta="center">
-          Here's a basic starter to show navigating from one screen to another. This screen uses the
-          same code on Next.js and React Native.
-        </Paragraph>
+      <Select value={val} onValueChange={setVal} disablePreventBodyScroll>
+        <Select.Trigger width={220} iconAfter={ChevronDown}>
+          <Select.Value placeholder="Something" />
+        </Select.Trigger>
 
-        <Separator />
-        <Paragraph ta="center">
-          Made by{' '}
-          <Anchor color="$color12" href="https://twitter.com/natebirdman" target="_blank">
-            @natebirdman
-          </Anchor>
-          ,{' '}
-          <Anchor
-            color="$color12"
-            href="https://github.com/tamagui/tamagui"
-            target="_blank"
-            rel="noreferrer"
+        <Adapt when="sm" platform="touch">
+          <Sheet
+            modal
+            dismissOnSnapToBottom
+            animationConfig={{
+              type: 'spring',
+              damping: 20,
+              mass: 1.2,
+              stiffness: 250,
+            }}
           >
-            give it a ⭐️
-          </Anchor>
-        </Paragraph>
-      </YStack>
+            <Sheet.Frame>
+              <Sheet.ScrollView>
+                <Adapt.Contents />
+              </Sheet.ScrollView>
+            </Sheet.Frame>
+            <Sheet.Overlay
+              animation="lazy"
+              enterStyle={{ opacity: 0 }}
+              exitStyle={{ opacity: 0 }}
+            />
+          </Sheet>
+        </Adapt>
 
-      <XStack>
-        <Button {...linkProps}>Link to user</Button>
-      </XStack>
+        <Select.Content zIndex={200000}>
+          <Select.ScrollUpButton
+            alignItems="center"
+            justifyContent="center"
+            position="relative"
+            width="100%"
+            height="$3"
+          >
+            <YStack zIndex={10}>
+              <ChevronUp size={20} />
+            </YStack>
+            <LinearGradient
+              start={[0, 0]}
+              end={[0, 1]}
+              fullscreen
+              colors={['$background', 'transparent']}
+              borderRadius="$4"
+            />
+          </Select.ScrollUpButton>
 
-      <SheetDemo />
+          <Select.Viewport
+            // to do animations:
+            // animation="quick"
+            // animateOnly={['transform', 'opacity']}
+            // enterStyle={{ o: 0, y: -10 }}
+            // exitStyle={{ o: 0, y: 10 }}
+            minWidth={200}
+          >
+            <Select.Group>
+              <Select.Label>Fruits</Select.Label>
+              {/* for longer lists memoizing these is useful */}
+              {useMemo(
+                () =>
+                  items.map((item, i) => {
+                    return (
+                      <Select.Item index={i} key={item.name} value={item.name.toLowerCase()}>
+                        <Select.ItemText>{item.name}</Select.ItemText>
+                        <Select.ItemIndicator marginLeft="auto">
+                          <Check size={16} />
+                        </Select.ItemIndicator>
+                      </Select.Item>
+                    )
+                  }),
+                [items]
+              )}
+            </Select.Group>
+          </Select.Viewport>
+
+          <Select.ScrollDownButton
+            alignItems="center"
+            justifyContent="center"
+            position="relative"
+            width="100%"
+            height="$3"
+          >
+            <YStack zIndex={10}>
+              <ChevronDown size={20} />
+            </YStack>
+            <LinearGradient
+              start={[0, 0]}
+              end={[0, 1]}
+              fullscreen
+              colors={['transparent', '$background']}
+              borderRadius="$4"
+            />
+          </Select.ScrollDownButton>
+        </Select.Content>
+      </Select>
     </YStack>
   )
 }
